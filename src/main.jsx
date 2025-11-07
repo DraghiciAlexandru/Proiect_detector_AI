bfdce3acc7c15d1f7d24307a89dd0d1d8590e2c7
unknown <manututelea@yahoo.com>
2025-11-07 12:28:38 +0200

    LOGIN

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<App />} />
+        <Route path="/signup" element={<SignUpPage />} /> {/* <-- new route */}
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
